fd787f75d360258b98054d962e8ee331ec1036cb
fantasyLh <m17671430910@163.com>
2023-06-29 17:28:07 +0800

    feat: 添加路由替换方法

--- src/routes/router.ts
+++ src/routes/router.ts
@@ -2,19 +2,34 @@ import Taro from "@tarojs/taro";
 import qs from 'qs';
 import { RouteName, routes } from ".";
 
+function joinParams(name, params?: Recordable) {
+    let url = routes[name];
+    if (params) {
+        url = `${url}?${qs.stringify(params)}`;
+    }
+    return url
+}
+
 class Router {
+    // 跳转到下一级
     static pushName(name: RouteName, params?: Recordable) {
-        let url = routes[name];
-        if (params) {
-            url = `${url}?${qs.stringify(params)}`;
-        }
+        const url = joinParams(name, params)
         Taro.navigateTo({
-            url: url,
+            url,
         })
     }
+    // 替换当前页面
+    static redirectName(name: RouteName, params?: Recordable) {
+        const url = joinParams(name, params)
+        Taro.redirectTo({
+            url
+        })
+    }
+    // 返回
     static back() {
         Taro.navigateBack();
     }
+    // 获取当前页面的参数
     static get params() {
         return Taro.getCurrentInstance().router?.params
     }
